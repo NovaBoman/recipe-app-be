725092dca378afb3d8e9dedb72955bb5e16eca84
Nova Boman <nova.boman@chasacademy.se>
2022-03-30 13:58:47 +0200

    Add title and foreign key to recipe lists table

--- recipe-app/database/migrations/2022_03_30_111733_create_recipe_lists_table.php
+++ recipe-app/database/migrations/2022_03_30_111733_create_recipe_lists_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('recipe_lists', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
         });
     }
 
