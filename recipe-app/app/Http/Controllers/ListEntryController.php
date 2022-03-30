451c79e707e2ea8e5e19eb9890ee5d1f61384ac4
Nova Boman <nova.boman@chasacademy.se>
2022-03-30 13:24:04 +0200

    Create ListEntry model, controller and migration

---
+++ recipe-app/app/Http/Controllers/ListEntryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ListEntry;
+use Illuminate\Http\Request;
+
+class ListEntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ListEntry  $listEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ListEntry $listEntry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ListEntry  $listEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ListEntry $listEntry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ListEntry  $listEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ListEntry $listEntry)
+    {
+        //
+    }
+}
